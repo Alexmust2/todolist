b1d6d1c2eb9a67b02fe5bf53a0a8138222a100f2
Alexmust2 <alex.must2003@mail.ru>
2023-09-15 15:48:01 +0300

    project init and add tasks groups

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,87 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="main_container">
+    <div 
+      class="todos_container"  
+      v-for="(list, index) in $store.state.todos" 
+      :key="index"
+    >
+        <h1 class="list_title" :style="{background:list.color}">{{ list.name }}</h1>
+        <ul>
+          <li 
+            v-for="task in list.tasks" 
+            :key="task.id"
+          >
+          {{ task.title }}
+        </li>
+        </ul>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import store from './store/index';
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  mounted(){
+    console.log(store.state.todos)
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+* {
+  margin: 0;
+  padding: 0;
+
+  box-sizing: border-box;
 }
+
+.main_container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+
+  height: 100vh;
+  width: 1920px;
+}
+
+.todos_container {
+  width: 350px;
+  height: 80%;
+
+  display: flex;
+  flex-direction: column;
+  align-self: center;
+
+  border: 2px solid black;
+  border-radius: 25px;
+  .list_title {
+    align-self: center;
+
+    width: 350px;
+    height: 50px;
+
+    border: 2px solid black;
+    border-radius: 25px 25px 0 0;
+
+    color: antiquewhite;
+
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+
+  }
+  ul {
+    padding: 15px;
+  }
+  li { 
+    list-style: none;
+  }
+}
+
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,33 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    todos:{
+      AgreementTodos:{
+        name: 'Agreement',
+        color: 'blue',
+        tasks: [{title: 'agree'}]
+      },
+      newTodos:{
+        name: 'New',
+        color: 'red',
+        tasks: [{title: 'new'}]
+      },
+      InProgressTodos:{
+        name: 'InProgress',
+        color: 'black',
+        tasks: [{title: 'inprogres'}]
+      },
+      ReadyTodos:{
+        name: 'Ready',
+        color: 'green',
+        tasks: [{title: 'ready'}]
+      },
+      FinalizeTodos:{
+        name: 'Final',
+        color: 'pink',
+        tasks: [{title: 'Final'}]
+      },
+    }
   },
   getters: {
   },
